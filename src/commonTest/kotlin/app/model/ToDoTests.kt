73e3498d36a9fdafd163f3bbcc346db729a88585
jamowei <jan@rexster.de>
2021-01-15 14:25:30 +0100

    fixed tests

--- src/commonTest/kotlin/app/model/ToDoTests.kt
+++ src/commonTest/kotlin/app/model/ToDoTests.kt
@@ -9,22 +9,12 @@ class ToDoTests {
         val validator = ToDoValidator()
 
         assertTrue(
-            validator.validate(ToDo(text = " start with space"), Unit).size == 1,
-            "validation for \"starting with space\" failed"
-        )
-
-        assertTrue(
             validator.validate(ToDo(text = "a"), Unit).size == 1,
             "validation for \"too short text\" failed"
         )
 
         assertTrue(
-            validator.validate(ToDo(text = " a"), Unit).size == 2,
-            "validation for \"two validations\" failed"
-        )
-
-        assertTrue(
-            validator.validate(ToDo(text = "a".repeat(101)), Unit).size == 1,
+            validator.validate(ToDo(text = "a".repeat(validator.maxTextLength + 1)), Unit).size == 1,
             "validation for \"max length\" failed"
         )
 
